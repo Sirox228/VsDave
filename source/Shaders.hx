03b0268358937992d214655977a3c0bf920c82f2
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-21 22:52:45 +0300

    Update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -468,39 +468,95 @@ class DitherShader extends FlxShader
 
         float Scale = 1.0;
         
-        int[8] getDith(int x)
+        void setDith(int x, int a[8])
         {
             if (x == 0) {
-        	return { 0, 32, 8, 40, 2, 34, 10, 42}; /* 8x8 Bayer ordered dithering */
+                a[0] = 0;
+                a[1] = 32; 
+                a[2] = 8;
+                a[3] = 40;
+                a[4] = 2;
+                a[5] = 34;
+                a[6] =  10;
+                a[7] = 42; /* 8x8 Bayer ordered dithering */
             }
             if (x == 1) {
-            	return {48, 16, 56, 24, 50, 18, 58, 26}; /* pattern. Each input pixel */
+            	a[0] = 48;
+                a[1] = 16;
+                a[2] = 56;
+                a[3] = 24;
+                a[4] = 50;
+                a[5] = 18;
+                a[6] = 58;
+                a[7] = 26; /* pattern. Each input pixel */
             }
             if (x == 2) {
-            	return {12, 44, 4, 36, 14, 46, 6, 38}; /* is scaled to the 0..63 range */
+            	a[0] = 12;
+                a[1] = 44;
+                a[2] = 4;
+                a[3] = 36;
+                a[4] = 14;
+                a[5] =46;
+                a[6] = 6;
+                a[7] = 38; /* is scaled to the 0..63 range */
             }
             if (x == 3) {
-            	return {60, 28, 52, 20, 62, 30, 54, 22}; /* before looking in this table */
+            	a[0] = 60;
+                a[1] = 28;
+                a[2] = 52;
+                a[3] = 20;
+                a[4] = 62;
+                a[5] = 30;
+                a[6] = 54;
+                a[7] = 22; /* before looking in this table */
             }
             if (x == 4) {
-            	return { 3, 35, 11, 43, 1, 33, 9, 41}; /* to determine the action. */
+                a[0] = 3;
+                a[1] = 35;
+                a[2] = 11;
+                a[3] = 43;
+                a[4] = 1;
+                a[5] = 33;
+                a[6] = 9;
+                a[7] = 41; /* to determine the action. */
             }
             if (x == 5) {
-            	return {51, 19, 59, 27, 49, 17, 57, 25};
+                a[0] = 51;
+                a[1] = 19;
+                a[2] = 59;
+                a[3] = 27;
+                a[4] = 49;
+                a[5] = 17;
+                a[6] = 57;
+                a[7] = 25;
             }
             if (x == 6) {
-            	return {15, 47, 7, 39, 13, 45, 5, 37};
+                a[0] = 15;
+                a[1] = 47;
+                a[2] = 7;
+                a[3] = 39;
+                a[4] = 13;
+                a[5] = 45;
+                a[6] = 5;
+                a[7] = 37;
             }
             if (x == 7) {
-            	return {63, 31, 55, 23, 61, 29, 53, 21};
+                a[0] = 63;
+                a[1] = 31;
+                a[2] = 55;
+                a[3] = 23;
+                a[4] = 61;
+                a[5] = 29;
+                a[6] = 53;
+                a[7] = 21;
             }
-            return {0,0,0,0,0,0,0,0};
         }
 
         float find_closest(int x, int y, float c0)
         {
 
-            int dither[8] = getDith(x);
+            int dither[8];
+            setDith(x, dither);
 
             float limit = 0.0;
             if(x < 8)
