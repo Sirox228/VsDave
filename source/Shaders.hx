1b63c9e4dc5ee1ff79bdcb57c8dec4c3228ab29f
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-21 08:55:44 +0300

    Update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -467,31 +467,51 @@ class DitherShader extends FlxVersionShader
         #pragma header
 
         float Scale = 1.0;
+        
+        int[8] getDith(int x)
+        {
+            if (x == 0) {
+        	return { 0, 32, 8, 40, 2, 34, 10, 42}; /* 8x8 Bayer ordered dithering */
+            }
+            if (x == 1) {
+            	return {48, 16, 56, 24, 50, 18, 58, 26}; /* pattern. Each input pixel */
+            }
+            if (x == 2) {
+            	return {12, 44, 4, 36, 14, 46, 6, 38}; /* is scaled to the 0..63 range */
+            }
+            if (x == 3) {
+            	return {60, 28, 52, 20, 62, 30, 54, 22}; /* before looking in this table */
+            }
+            if (x == 4) {
+            	return { 3, 35, 11, 43, 1, 33, 9, 41}; /* to determine the action. */
+            }
+            if (x == 5) {
+            	return {51, 19, 59, 27, 49, 17, 57, 25};
+            }
+            if (x == 6) {
+            	return {15, 47, 7, 39, 13, 45, 5, 37};
+            }
+            if (x == 7) {
+            	return {63, 31, 55, 23, 61, 29, 53, 21};
+            }
+            return {0,0,0,0,0,0,0,0};
+        }
 
         float find_closest(int x, int y, float c0)
         {
 
-        int dither[8][8] = {
-        { 0, 32, 8, 40, 2, 34, 10, 42}, /* 8x8 Bayer ordered dithering */
-        {48, 16, 56, 24, 50, 18, 58, 26}, /* pattern. Each input pixel */
-        {12, 44, 4, 36, 14, 46, 6, 38}, /* is scaled to the 0..63 range */
-        {60, 28, 52, 20, 62, 30, 54, 22}, /* before looking in this table */
-        { 3, 35, 11, 43, 1, 33, 9, 41}, /* to determine the action. */
-        {51, 19, 59, 27, 49, 17, 57, 25},
-        {15, 47, 7, 39, 13, 45, 5, 37},
-        {63, 31, 55, 23, 61, 29, 53, 21} };
+            int dither[8] = getDith(x);
 
-        float limit = 0.0;
-        if(x < 8)
-        {
-            limit = (dither[x][y]+1)/64.0;
-        }
+            float limit = 0.0;
+            if(x < 8)
+            {
+                limit = (dither[y]+1)/64.0;
+            }
 
-
-        if(c0 < limit)
-            return 0.0;
-            return 1.0;
-        }
+            if(c0 < limit)
+                return 0.0;
+                return 1.0;
+            }
         }
 
         void main(void)
